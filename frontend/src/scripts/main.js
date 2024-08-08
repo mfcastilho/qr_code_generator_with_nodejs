908334740a049d87f4bc092ce65d71b436034d16
mfcastilho <fredericocastilho@hotmail.com>
2024-08-08 02:41:43 -0300

    feat: create a simple frontend

---
+++ frontend/src/scripts/main.js
@@ -0,0 +1,32 @@
+console.log('Entrou');
+import axios from 'axios';
+
+
+const url = document.querySelector('.qrcode-address-form__input');
+console.log(url);
+
+const button = document.querySelector('.qrcode-address-form__button');
+
+button.addEventListener('click', handleSubmit);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  console.log(url.value);
+  
+  if (url) {
+    try {
+      const response = await axios.post('http://localhost:5050/create-qrcode', {
+        url: url.value
+      });
+  
+      console.log(response.data.message);
+      
+    } catch (error) {
+      console.log(error.data.message);
+      
+    }
+ }
+}
+
+
